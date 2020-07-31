b18e2bc0fb955885f2f3a65cce17cd7447719503
Jesse Aldana <jesse.aldana@gmail.com>
2020-07-31 16:51:21 -0700

    Updates

---
+++ precache-manifest.51d398d8c6e5b9479d1d7b219340d077.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac2027f2d0b5abf0e7e0a695e4edce9",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5b19570a4b0d00eeaaaf",
+    "url": "/Portfolio/static/css/main.64e3fd7e.chunk.css"
+  },
+  {
+    "revision": "6dcc17804074385c9efa",
+    "url": "/Portfolio/static/js/2.808de669.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.808de669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b19570a4b0d00eeaaaf",
+    "url": "/Portfolio/static/js/main.05abb10b.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "3bf0ec99bb999afebca703aabf58d260",
+    "url": "/Portfolio/static/media/aboutBackground.3bf0ec99.jpg"
+  },
+  {
+    "revision": "93e0a4a9cc4698213d3ffabfd3cf7b0a",
+    "url": "/Portfolio/static/media/boardgame.93e0a4a9.png"
+  },
+  {
+    "revision": "3169f265c78517fa86d52ff91c74b19e",
+    "url": "/Portfolio/static/media/budget.3169f265.png"
+  },
+  {
+    "revision": "71cb40d0293575ebd6681725f5d03a8f",
+    "url": "/Portfolio/static/media/burgerCompendium.71cb40d0.png"
+  },
+  {
+    "revision": "1bb62160ee862df0154bc49162e83618",
+    "url": "/Portfolio/static/media/dinnerAndAmovie.1bb62160.jpg"
+  },
+  {
+    "revision": "e38e173d623eabdcb3ebebf6f627db5a",
+    "url": "/Portfolio/static/media/fitness.e38e173d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aed515b2c03df0d2343a86e487dd521c",
+    "url": "/Portfolio/static/media/passwordGenerator.aed515b2.jpg"
+  },
+  {
+    "revision": "989f6c97854ea9b51927f304b80efd0c",
+    "url": "/Portfolio/static/media/selfie.989f6c97.jpg"
+  }
+]);
