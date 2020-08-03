506c39a985e75bf024a9ed43a64a5e4632435f3a
Jesse Aldana <jesse.aldana@gmail.com>
2020-08-03 09:52:03 -0700

    updated portfolio

--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -3,7 +3,7 @@ import dinner from "./Images/dinnerAndAmovie.jpg";
 import password from "./Images/passwordGenerator.jpg";
 import boardgame from "./Images/boardgame.png";
 import burger from "./Images/burgerCompendium.png";
-import fitness from "./Images/fitness.png";
+import book from "./Images/imaginaryTraveler.png"
 import budget from "./Images/budget.png";
 import Container from "../Container";
 
@@ -30,11 +30,11 @@ function Portfolio() {
 
         <div className="row">
           <div className="col-sm-6 PortfolioCard">
-            <a href="https://sharlenemay.github.io/dinner_movie/" target="_blank">
-            <img src={dinner} className="img-fluid" alt="Dinner and a Movie"/>
+            <a href="https://imaginarytraveler4.herokuapp.com/" target="_blank">
+            <img src={book} className="img-fluid" alt="Imaginary Traveler"/>
             </a>
             <div id="caption">
-              Movie and Foodie App
+              Virtual Walkabout
             </div>
           </div>  
           <div className="col-sm-6 PortfolioCard">
@@ -56,7 +56,7 @@ function Portfolio() {
               Boardgame
             </div>
           </div>
-          <div className="col-sm-6 PortfoloCard">
+          <div className="col-sm-6 PortfolioCard">
             <a href="https://shrouded-forest-02182.herokuapp.com/" target="_blank">           
               <img src={burger} className="img-fluid" alt="Burger Compendium"/>
             </a>
@@ -67,14 +67,14 @@ function Portfolio() {
         </div>
 
         <div className="row">
-          <div className="col-sm-6 PortfolioCard">
-            <a href="https://vast-eyrie-34582.herokuapp.com/?id=5eed4b160dcd3a00176bebd6" target="_blank">
-              <img src={fitness} className="img-fluid" alt="Fitness Tracker"/>
+        <div className="col-sm-6 PortfolioCard">
+            <a href="https://sharlenemay.github.io/dinner_movie/" target="_blank">
+            <img src={dinner} className="img-fluid" alt="Dinner and a Movie"/>
             </a>
             <div id="caption">
-              Fitness Tracker
+              Movie and Foodie App
             </div>
-          </div>
+          </div>  
           <div className="col-sm-6 PortfolioCard">
             <a href="https://fierce-falls-45988.herokuapp.com/" target="_blank">           
               <img src={budget} className="img-fluid" alt="Budget Tracker"/>
